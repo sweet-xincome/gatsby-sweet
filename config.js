6e86a68d00cc20ee275cce5ef679c1483c7dd226
Jing <glnewworld@outlook.com>
2020-01-17 15:32:40 +0800

    GitHub pages配置更改

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   url: 'https://lumen.netlify.com',
-  pathPrefix: '/',
+  pathPrefix: '/gatsby-sweet',
   title: 'Blog by John Doe',
   subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
   copyright: '© All rights reserved.',
